767cdc66737e886fcf42bede2a6765fb26701914
MrGeislinger <GitHub@MrGeislinger.com>
2019-09-13 16:03:13 -0700

    feat: Update API notebook w/info

---
+++ DataEngineering/APIs/secrets.py
@@ -0,0 +1,17 @@
+# ExampleTokenDemo-20190415
+class Lifx():
+    tokens = {
+        '20190415' : {
+            'token_name': 'ExampleTokenDemo-20190415',
+            'token': 'random-letters-and-junk',
+            'expired': True
+        },
+        '20190529' : {
+            'token_name': 'ExampleTokenDemo-20190529',
+            'token': 'random-letters-and-junk',
+            'expired': True
+        }
+    }
+    
+    # Default token
+    default_token = tokens['20190529']
